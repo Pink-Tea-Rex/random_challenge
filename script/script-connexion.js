37dc2beec5dea0b0142cb8658ec1f1fb8b0d9058
FLORE BRUNELIERE <flore.bruneliere2024@campus-eni.fr>
2024-11-19 16:46:25 +0100

    try verficationConexion function

--- script/script-connexion.js
+++ script/script-connexion.js
@@ -1,9 +1,10 @@
-/******************** voir le pswd   ********************/ 
+/******************** voir le pswd   ********************/
+
 //je récupère la checkbox
 let eye = document.getElementById("visibility-pswd");
 //console.log(eye);
 //j'ajoute addEnventListener pour voir si c'est check ou pas
-eye.addEventListener("change", function() {
+eye.addEventListener("change", function () {
     //console.log("change");
     //je récupère l'input du pswd
     let displayStatut = document.getElementById("pswd");
@@ -21,23 +22,22 @@ eye.addEventListener("change", function() {
 
 /******************** récupérer les data session storage ********************/
 
-
 let registerForm = document.getElementById("register-form");
- 
-registerForm.addEventListener("submit", function(event){
+//je récupère les valeurs qui m'intéresse
+let userName = document.getElementById("username").value;
+let email = document.getElementById("mail").value;
+let pswd = document.getElementById("pswd").value;
+let confirmedPswd = document.getElementById("confirmed-pswd").value;
+
+registerForm.addEventListener("submit", function (event) {
     verification(event)
 });
 
-function verification(event){
+function verification(event) {
     event.preventDefault();
 
     //preventDefault = empêche l'action de submit
-    //je récupère les valeurs qui m'intéresse
-    let userName = document.getElementById("username").value;
-    let email = document.getElementById("mail").value;
-    let pswd = document.getElementById("pswd").value;
-    let confirmedPswd = document.getElementById("confirmed-pswd").value;
- 
+
     // je vérifie que les pswd soient identiques
     if (pswd === confirmedPswd) {
         //alert("Votre inscription a bien été enregistrée !");
@@ -47,15 +47,32 @@ function verification(event){
         localStorage.setItem("email", email);
         localStorage.setItem("mot de passe", pswd);
         // je redirige vers la page login si connexion réussi
-        document.location.href="login.html";
-        
-    } else {        
+        document.location.href = "login.html";
+
+    } else {
         console.log("les mots de passe sont différents !")
     }
-}
+};
 
 
-    
+let loginForm = document.getElementById("login-form");
 
+loginForm.addEventListener("submit", function (event) {
+    verificationConnexion(event)
+});
+
+function verificationConnexion(event) {
+    event.preventDefault();
+
+    let mailLogin = document.getElementById("mail-login").value;
+    let pswdLogin = document.getElementById("passeword-login").value;
+
+    if (mailLogin === userName && pswdLogin === pswd) {
+        alert("connexion réussie !");
+        document.location.href = "index.html";
+    } else {
+        alert("connexion échouée");
+    }
+};
 
 
